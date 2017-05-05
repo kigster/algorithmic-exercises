1cd5caa8ea84b815bbd9107de2a684d5a60cead4
Konstantin Gredeskoul <kig@reinvent.one>
2017-05-05 10:36:03 -0700

    Reducing # of iterations per second required for slow

--- ruby/spec/interview/ruby/equilibrium/finder_slow_spec.rb
+++ ruby/spec/interview/ruby/equilibrium/finder_slow_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require_relative 'shared_examples'
 
 RSpec.describe Interview::Ruby::Equilibrium::FinderSlow do
-  include_examples :equilibrium, Interview::Ruby::Equilibrium::FinderSlow, 15
+  include_examples :equilibrium, Interview::Ruby::Equilibrium::FinderSlow, 10
 end
 
 
